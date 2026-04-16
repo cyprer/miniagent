2509b14e2c069d5b7905363ae4e0bb2b642dc15a
ccg <chencaiguang@highlander.com.cn>
2026-04-16 09:55:30 +0800

    整合 DuckDuckGo 搜索工具到智能助手中并添加一些系统提示词

--- mini_agent.py
+++ mini_agent.py
@@ -6,6 +6,7 @@ from langchain.agents import create_agent
 from langchain.messages import AIMessageChunk
 import datetime
 from langchain.tools import tool
+from langchain_community.tools import DuckDuckGoSearchRun
 
 load_dotenv()
 
@@ -19,19 +20,26 @@ model = ChatAnthropic(
 
 checkpointer = InMemorySaver()
 
-# 定义获取当前系统时间的tool
 @tool
 def get_current_time() -> str:
     """获取当前系统时间，格式为YYYY-MM-DD HH:MM:SS"""
     now = datetime.datetime.now()
     return now.strftime("%Y-%m-%d %H:%M:%S")
 
-agent = create_agent(model, tools=[get_current_time], checkpointer=checkpointer)
+web_search = DuckDuckGoSearchRun(name="web_search", description="搜索互联网获取最新信息，当你需要查询实时新闻、天气预报、股票价格、实时比赛结果或其他最新数据时使用此工具")
+
+SYSTEM_PROMPT = """你是一个智能助手。每次调用 web_search 搜索工具之前，**必须先调用 get_current_time 工具获取当前时间**，只有知道当前时间才能提供准确的信息。
+
+重要规则：
+- 调用 web_search 之前，先调用 get_current_time
+- 返回结果时你应该标注信息的来源以及发布时间
+- 遇到你不确定的信息你应该告知用户或者你需要用户提供一些信息你应该及时询问
+"""
+
+agent = create_agent(model, tools=[get_current_time, web_search], checkpointer=checkpointer, system_prompt=SYSTEM_PROMPT)
 
 print("对话已启动（输入 exit/quit/q 退出）:\n")
-now = datetime.datetime.now()
 config = {"configurable": {"thread_id": "1"}}
-print(now.strftime("%Y-%m-%d %H:%M:%S"))
 while True:
     user_input = input("你: ").strip()
     if user_input.lower() in ["exit", "quit", "q", "退出"]:
